0f8d48558addbc153d9deabc49621616280262c2
Dreams0809 <expensivepain9@gmail.com>
2022-11-28 21:32:45 -0500

    changes

--- public/index.js
+++ public/index.js
@@ -1,12 +1,10 @@
-﻿
-
-document.querySelector('#search').addEventListener('click',getEducator)
+﻿document.querySelector('#search').addEventListener('click',getEducator)
 const searchField = document.querySelector('#subject')
 
 function getEducator(){
     const input = searchField.value
 
-    fetch('/subject/'+input)
+    fetch('/subject/:subject'+input)
     .then(res => res.json())
     .then(data => {
         console.log(data)
@@ -14,6 +12,7 @@ function getEducator(){
 
 
     })
+
 }
 
 
